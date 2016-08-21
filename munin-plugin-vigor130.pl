8b8637b9be337ff73fdda5e785694ef626dae9b3
Michael 'PoempelFox' Meier <git@mulder.franken.de>
2016-08-21 21:29:49 +0200

    add graph with lots of error counters

--- munin-plugin-vigor130.pl
+++ munin-plugin-vigor130.pl
@@ -85,7 +85,7 @@ if ((@ARGV > 0) && ($ARGV[0] eq "config")) {
   print("graph_category VDSL\n");
   print("graph_title VDSL Data Rate\n");
   print("graph_args --lower-limit 0\n");
-  print("graph_vlabel kbps\n");
+  print("graph_vlabel bps\n");
   print("attdnrate.label attainable downstream rate\n");
   print("attdnrate.type GAUGE\n");
   print("attdnrate.draw LINE0.6\n");
@@ -129,10 +129,95 @@ if ((@ARGV > 0) && ($ARGV[0] eq "config")) {
   print("attenuationup.draw LINE2.0\n");
   print("attenuationup.info The attenuation on the upstream.\n");
   
+  # potential FIXME: These are not in the same units, so a graph combining them
+  # all will probably scale wrong. Also, their proportions will depend on the
+  # actual line. It might not be possible to combine those all into one graph.
+  print("multigraph vig130_errors1_dn\n");
+  print("graph_category VDSL\n");
+  print("graph_title VDSL Errors Downstream\n");
+  print("graph_vlabel n\n");
+  print("crcerrsdn.label CRC errors\n");
+  print("crcerrsdn.type DERIVE\n");
+  print("crcerrsdn.min 0\n");
+  print("crcerrsdn.draw LINE2.0\n");
+  print("crcerrsdn.info The number of CRC (Cyclic Redundancy Check) errors on the" .
+        " downstream. Those are corrupted and thus lost packets that required" .
+        " resubmission, so these are bad.\n");
+  print("fecsdn.label FECS\n");
+  print("fecsdn.type DERIVE\n");
+  print("fecsdn.min 0\n");
+  print("fecsdn.draw LINE2.0\n");
+  print("fecsdn.info The number of FECS (Forward Error Correction Seconds) on" .
+        " the downstream, i.e. in how many seconds there were errors corrected" .
+        " by FEC. Those are errors that could be fully corrected from" .
+        " redundancy information by the modem, nothing was lost. These do not" .
+        " have a negative impact on performance, so they are pretty much" .
+        " irrelevant.\n");
+  print("esdn.label ES\n");
+  print("esdn.type DERIVE\n");
+  print("esdn.min 0\n");
+  print("esdn.draw LINE2.0\n");
+  print("esdn.info The number of ES (Errored Seconds) on the downstream," .
+        " i.e. in how many seconds there were errors encountered\n");
+  print("sesdn.label SES\n");
+  print("sesdn.type DERIVE\n");
+  print("sesdn.min 0\n");
+  print("sesdn.draw LINE2.0\n");
+  print("sesdn.info The number of SES (Severely Errored Seconds) on the downstream\n");
+  print("lossdn.label LOSS\n");
+  print("lossdn.type DERIVE\n");
+  print("lossdn.min 0\n");
+  print("lossdn.draw LINE2.0\n");
+  print("lossdn.info The number of LOSS (Loss Of Signal Seconds) on the downstream\n");
+  print("uasdn.label UAS\n");
+  print("uasdn.type DERIVE\n");
+  print("uasdn.min 0\n");
+  print("uasdn.draw LINE2.0\n");
+  print("uasdn.info The number of UAS (UnAvailable Seconds) on the downstream\n");
+  
+  print("multigraph vig130_errors1_up\n");
+  print("graph_category VDSL\n");
+  print("graph_title VDSL Errors Upstream\n");
+  print("graph_vlabel n\n");
+  print("crcerrsup.label CRC errors\n");
+  print("crcerrsup.type DERIVE\n");
+  print("crcerrsup.min 0\n");
+  print("crcerrsup.draw LINE2.0\n");
+  print("crcerrsup.info The number of CRC (Cyclic Redundancy Check) errors on the upstream.\n");
+  print("fecsup.label FECS\n");
+  print("fecsup.type DERIVE\n");
+  print("fecsup.min 0\n");
+  print("fecsup.draw LINE2.0\n");
+  print("fecsup.info The number of FECS (Forward Error Correction Seconds) on" .
+        " the upstream.\n");
+  print("esup.label ES\n");
+  print("esup.type DERIVE\n");
+  print("esup.min 0\n");
+  print("esup.draw LINE2.0\n");
+  print("esup.info The number of ES (Errored Seconds) on the upstream\n");
+  print("sesup.label SES\n");
+  print("sesup.type DERIVE\n");
+  print("sesup.min 0\n");
+  print("sesup.draw LINE2.0\n");
+  print("sesup.info The number of SES (Severely Errored Seconds) on the upstream\n");
+  print("lossup.label LOSS\n");
+  print("lossup.type DERIVE\n");
+  print("lossup.min 0\n");
+  print("lossup.draw LINE2.0\n");
+  print("lossup.info The number of LOSS (Loss Of Signal Seconds) on the upstream\n");
+  print("uasup.label UAS\n");
+  print("uasup.type DERIVE\n");
+  print("uasup.min 0\n");
+  print("uasup.draw LINE2.0\n");
+  print("uasup.info The number of UAS (UnAvailable Seconds) on the upstream\n");
+
   # PSD should be constant unless the profile on the DSLAM is changed.
-  # It also isn't clear how this value is corrected, as it has to be an average
+  # It also isn't clear how this value is calculated, as it has to be an average
   # over a lot of values? Lets not draw it for now.
   #print("snrmargindn.info PSD is the power spectrum density\n");
+  
+  # NFEC: Actual Size of Reed-Solomon Codeword
+  # RFEC: Actual number of Reed-Solomon redundancy bytes.
   
   exit(0);
 }
@@ -154,6 +239,18 @@ my $snrmargindn = 'U';
 my $snrmarginup = 'U';
 my $attenuationdn = 'U';
 my $attenuationup = 'U';
+my $crcerrsdn = 'U';
+my $crcerrsup = 'U';
+my $fecsdn = 'U';
+my $fecsup = 'U';
+my $esdn = 'U';
+my $esup = 'U';
+my $sesdn = 'U';
+my $sesup = 'U';
+my $lossdn = 'U';
+my $lossup = 'U';
+my $uasdn = 'U';
+my $uasup = 'U';
 if ($dslsp =~ m!Attainable Rate</td><td>(.*?)</td><td>.*?</td><td>(.*?)</td>!) {
   $attdnrate = int($1);
   $attuprate = int($2);
@@ -170,6 +267,30 @@ if ($dslsp =~ m!Attenuation</td><td>(.*?)</td><td>.*?</td><td>(.*?)</td>!) {
   $attenuationdn = $1;
   $attenuationup = $2;
 }
+if ($dslsp =~ m!<td>CRC</td><td>(.*?)</td><td>.*?</td><td>(.*?)</td>!) {
+  $crcerrsdn = int($1);
+  $crcerrsup = int($2);
+}
+if ($dslsp =~ m!FECS</td><td>(.*?)</td><td>.*?</td><td>(.*?)</td>!) {
+  $fecsdn = int($1);
+  $fecsup = int($2);
+}
+if ($dslsp =~ m!<td>ES</td><td>(.*?)</td><td>.*?</td><td>(.*?)</td>!) {
+  $esdn = int($1);
+  $esup = int($2);
+}
+if ($dslsp =~ m!<td>SES</td><td>(.*?)</td><td>.*?</td><td>(.*?)</td>!) {
+  $sesdn = int($1);
+  $sesup = int($2);
+}
+if ($dslsp =~ m!<td>LOSS</td><td>(.*?)</td><td>.*?</td><td>(.*?)</td>!) {
+  $lossdn = int($1);
+  $lossup = int($2);
+}
+if ($dslsp =~ m!<td>UAS</td><td>(.*?)</td><td>.*?</td><td>(.*?)</td>!) {
+  $uasdn = int($1);
+  $uasup = int($2);
+}
 print("multigraph vig130_datarates\n");
 print("attdnrate.value $attdnrate\n");
 print("attuprate.value $attuprate\n");
@@ -181,3 +302,17 @@ print("snrmarginup.value $snrmarginup\n");
 print("multigraph vig130_attenuation\n");
 print("attenuationdn.value $attenuationdn\n");
 print("attenuationup.value $attenuationup\n");
+print("multigraph vig130_errors1_dn\n");
+print("crcerrsdn.value $crcerrsdn\n");
+print("fecsdn.value $fecsdn\n");
+print("esdn.value $esdn\n");
+print("sesdn.value $sesdn\n");
+print("lossdn.value $lossdn\n");
+print("uasdn.value $uasdn\n");
+print("multigraph vig130_errors1_up\n");
+print("crcerrsup.value $crcerrsup\n");
+print("fecsup.value $fecsup\n");
+print("esup.value $sesup\n");
+print("sesup.value $sesup\n");
+print("lossup.value $lossup\n");
+print("uasup.value $uasup\n");
